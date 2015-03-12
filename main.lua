e4dcd2b09ca9cd3af5461a0ab139f90a23ac8cf8
Igor Rocha <igortice@gmail.com>
2015-03-12 20:23:17 -0300

    add fase 1 and scene

--- main.lua
+++ main.lua
@@ -1,5 +1,21 @@
-local function main(  )
-  display.newText("Hello World", display.contentCenterX, display.contentCenterY, native.systemFont, 36)
+local function main( )
+  -- hidden status bar
+  display.setStatusBar( display.HiddenStatusBar )
+
+  -- set background
+  local background = display.newImage( "images/furacao.png", display.contentCenterX, display.contentCenterY )
+
+  local texto_iniciar = display.newText( "Iniciar Jogo", display.contentCenterX, display.contentCenterY, native.systemFont, 24 )
+  texto_iniciar:setFillColor( 0,0,1 )
+
+  local function fase_um( event )
+    if ( event.phase == "began" ) then
+      local composer = require( "composer" )
+      composer.gotoScene( "fase1", { effect = "fade", time = 500 } )
+    end
+  end
+
+  texto_iniciar:addEventListener( "touch", fase_um )
 end
 
 main()
