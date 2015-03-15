249e42c0ccfcdc40005f7521667c977ff1c48e4d
Igor Rocha <igortice@gmail.com>
2015-03-15 14:25:49 -0300

    init tela inicial

--- main.lua
+++ main.lua
@@ -2,20 +2,11 @@ local function main( )
   -- hidden status bar
   display.setStatusBar( display.HiddenStatusBar )
 
-  -- set background
-  local background = display.newImage( "images/furacao.png", display.contentCenterX, display.contentCenterY )
+  -- require controller module
+  local composer = require "composer"
 
-  local texto_iniciar = display.newText( "Iniciar Jogo", display.contentCenterX, display.contentCenterY, native.systemFont, 24 )
-  texto_iniciar:setFillColor( 0,0,1 )
-
-  local function fase_um( event )
-    if ( event.phase == "began" ) then
-      local composer = require( "composer" )
-      composer.gotoScene( "fase1", { effect = "fade", time = 500 } )
-    end
-  end
-
-  texto_iniciar:addEventListener( "touch", fase_um )
+  -- load first scene
+  composer.gotoScene( "tela_inicial", { effect = "fade", time = 500 } )
 end
 
 main()
